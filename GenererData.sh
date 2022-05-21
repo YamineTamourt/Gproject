859a5a402c1a481795993ca962ffdb9798a31a9f
YamineTamourt <102610049+YamineTamourt@users.noreply.github.com>
2022-05-21 13:30:25 +0200

    Update GenererData.sh

--- GenererData.sh
+++ GenererData.sh
@@ -1,3 +1,8 @@
 "!/bin/bash
 
 for video in $(youtube-dl -j --flat-playlist $1 | jq -r '.id' | sed 's_^_https://youtu.be/_')
+do
+  youtube-dl -J $video  | jq '{"date": .upload_date,"title": .title,"author": .uploader,"description": .description}' | grep ":"
+  
+  youtude-de --get-filename $video
+done
