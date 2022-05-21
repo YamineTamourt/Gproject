55d1be011de6386d4a3e9ebc3f42b8b686168a6f
YamineTamourt <102610049+YamineTamourt@users.noreply.github.com>
2022-05-21 15:45:39 +0200

    Update GenererData.sh

--- GenererData.sh
+++ GenererData.sh
@@ -2,11 +2,11 @@
 
 for video in $(youtube-dl -j --flat-playlist $1 | jq -r '.id' | sed 's_^_https://youtu.be/_')
 do
-  youtube-dl -J $video  | jq '{"date": .upload_date,"title": .title,"author": .uploader,"description": .description}' | grep ":" > file.txt
+  youtube-dl -J $video  | jq '{"date": .upload_date,"title": .title,"author": .uploader,"description": .description, "null"}' | grep ":" > file.txt
   
-  while IFS =: read name data
+  while IFS=: read name data
   do
-    echo $data
+    echo ${data:2:${#data}-4}
   done < file.txt
   
   youtude-de --get-filename $video
