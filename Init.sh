ced1651d68be20f7bc3bcd11d8ae83151a887b61
YamineTamourt <102610049+YamineTamourt@users.noreply.github.com>
2022-05-22 14:29:48 +0200

    Update Init.sh

--- Init.sh
+++ Init.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
-#
+
 echo "Entrer le lien de la playlist pour generer la page (vide pour generer la page d'exemple)"
-#
+
 read lien
-#
+
 if [[ -z $lien ]]
 then
 	echo "generation de la page d'exemple ..."
-#	
+	
 	/usr/sbin/apache2ctl -DFOREGROUND
-#
+
 	exit 0
 fi
-#
+
 youtube-dl -s $lien > /dev/null 2> /dev/null
-#
+
 if [ $? -ne 0 ]
 then
 	echo "lien invalide"
 	echo "generation de la page d'exemple ..."
 else
 	echo "telechargement de la playlist ..."
-#
+
 	youtube-dl -o '/var/www/html/video' $lien
-#	
+	
 	echo "generation de la page ..."
-#	
+	
 	chmod +rm GenererData.sh
 	javac /var/www/html/GenerateurHtml.java
-#	
+	
 	./GenererData.sh $lien
 	java var/www/html/GenerateurHtml
 fi
-#
+
 /usr/sbin/apache2ctl -DFOREGROUND
-#
+
 exit 0
