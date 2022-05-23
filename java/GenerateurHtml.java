00bc3889e48a0b938d9a4904f07e8619af71723e
YamineTamourt <102610049+YamineTamourt@users.noreply.github.com>
2022-05-23 17:02:00 +0200

    Update GenerateurHtml.java

--- java/GenerateurHtml.java
+++ java/GenerateurHtml.java
@@ -51,13 +51,15 @@ public class GenerateurHtml
 			
 			pw.println ( "\t\t\t<tr>" );
 
-			pw.println ( "\t\t\t\t<td> <video width=\"320\" height=\"240\" controls> <source src=\"video/" + v.getFichier() + "\" type=\"video/mp4\"> </video> </td>" );
+			pw.println ( "\t\t\t\t<td> <video width=\"500\" height=\"240\" controls> <source src=\"video/" + v.getFichier() + "\" type=\"video/mp4\"> </video> </td>" );
 		
 			pw.println ( "\t\t\t</tr>" );
 			
 			pw.println ( "\t\t\t<tr>" );
 			
 			pw.println ( "\t\t\t\t<td> Made by " + v.getAuteur()  +"                       " + v.getDate() + " </td>" );
+			
+			pw.println ( "\t\t\t\t</br>" );
 
 			pw.println ( "\t\t\t</tr>" );
 			
